135f0bbba9082f11ed5b9192fb6868d72cef7465
itu-itis25-huseynli25 <huseynlisarkhan02@gmail.com>
2026-01-30 12:13:58 +0400

    Add files via upload

---
+++ rockscissorspaper.py
@@ -0,0 +1,27 @@
+import random
+
+emojis = {'r': '🪨', 's': '✂️', 'p': '📄'}
+choices = ('r', 's', 'p')
+while True:
+ user_choice = input("Rock, scissors or paper? (r/p/s): ").lower()
+ if user_choice not in choices:
+     print("Invalid choice!")
+     continue
+ computer_choice = random.choice(choices)
+
+ print(f'You chose {emojis[user_choice]}')
+ print(f'Computer chose {emojis[computer_choice]}')
+
+ if user_choice == computer_choice:
+    print('Tie!')
+ elif (
+     (user_choice == 'r' and computer_choice == 's') or
+     (user_choice == 's' and computer_choice == 'p') or
+     (user_choice == 'p' and computer_choice == 'r')):
+     print("You win!")
+ else:
+     print('You lose!')
+
+ should_continue = input('Continue? (y/n): ').lower()
+ if should_continue == 'n':
+    break
